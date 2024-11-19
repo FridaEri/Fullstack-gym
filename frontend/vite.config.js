160b393250a647f02b090386e0a4cb6c8096f904
Frida Ericsson <fridaericsson@live.com>
2024-11-19 12:23:00 +0100

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
+      '/api': {
+        target: 'http://74.241.242.179:3000', // Använd rätt backend-URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Tar bort "/api" från proxade URL:er
+      },
+    },
+  },
 });
