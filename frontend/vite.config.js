3aef50d643d725b2642232c945cc5ac0305eb5d9
Frida Ericsson <fridaericsson@live.com>
2024-11-19 13:15:20 +0100

    changed back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: '74.241.242.179:3000', // Backend server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+      '/api': 'http://localhost:3000'
+    }
+  }
+})
